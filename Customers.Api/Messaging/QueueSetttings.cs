1f85970ae45c001dad7047c7a107d19a23c80a87
Erbynn John <john.erbynn@gmail.com>
2024-07-14 11:53:28 -0500

    add sqs messaging

---
+++ Customers.Api/Messaging/QueueSetttings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customers.Api.Messaging
+{
+    public class QueueSetttings
+    {
+        public required string QueueName { get; set; }
+    }
+}
---
+++ SQS/SqsConsumerClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var sqsClient = new AmazonSQSClient();
+var cts = new CancellationTokenSource();
+
+var queueUrl = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrl.QueueUrl,
+    AttributeNames = new List<string> { "All" }, // show 'attribute' names attached to message by producer
+    MessageAttributeNames = new List<string>{ "All" } // show 'message details' from aws dashboard
+};
+
+
+while(!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(queueUrl.QueueUrl, message.ReceiptHandle, cts.Token);
+    }
+
+    await Task.Delay(1000);
+}
+
+// System.Console.WriteLine(response);
