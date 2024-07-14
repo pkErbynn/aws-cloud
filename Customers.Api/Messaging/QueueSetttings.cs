8c0480b27a0642934c2c9233b418b9cb07059474
Erbynn John <john.erbynn@gmail.com>
2024-07-14 17:10:18 -0500

    add SQS message publishing

--- Customers.Api/Messaging/QueueSetttings.cs
+++
@@ -1,8 +0,0 @@
-namespace Customers.Api.Messaging
-{
-    public class QueueSetttings
-    {
-        public const string Key = "QueueSettings";  // for section key
-        public required string QueueName { get; set; }
-    }
-}
