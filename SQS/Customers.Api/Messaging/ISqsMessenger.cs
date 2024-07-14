68b29ce3b315f8fac2ed0f486f6f7e95f2f465dc
Erbynn John <john.erbynn@gmail.com>
2024-07-14 17:09:18 -0500

    add SQS message publishing

---
+++ SQS/Customers.Api/Messaging/ISqsMessenger.cs
@@ -0,0 +1,9 @@
+using Amazon.SQS.Model;
+
+namespace Customers.Api.Messaging
+{
+    public interface ISqsMessenger
+    {
+        Task<SendMessageResponse> SendMessageAsync<T>(T message);
+    }
+}
